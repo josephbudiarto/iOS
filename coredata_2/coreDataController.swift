c68bd59d8c0403bac1bbbcdff53098883208b335
IOS <ios@SC-011s-Mac-mini.local>
2017-04-13 10:09:32 +0700

    coredata & table view

--- coredata_2/coreDataController.swift
+++ coredata_2/coreDataController.swift
@@ -1,25 +1,19 @@
-//
-//  ViewController.swift
-//  coredata_2
-//
-//  Created by IOS on 4/13/17.
-//  Copyright © 2017 IOS. All rights reserved.
-//
-
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class coreDataController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var userText: UITextField!
-    
     @IBOutlet weak var passwordText: UITextField!
-    
-    
+    @IBOutlet weak var newUserText: UITextField!
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var newPassText: UITextField!
+    var userArr:[users]=[]
     override func viewDidLoad() {
         super.viewDidLoad()
+        isi()
     }
-    
+
     @IBAction func insertButton(_ sender: UIButton) {
         guard let appDel: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
@@ -38,7 +32,21 @@ class ViewController: UIViewController {
         } catch {
             print("ada error")
         }
-        //retrieve the data
+        userText.text = "" //reset isi textfield untuk isian berikutnya
+        passwordText.text = ""  //reset isi textfield untuk isian berikutnya
+        isi()
+        table.reloadData()
+
+    }
+    @IBAction func editButton(_ sender: UIButton) {
+        guard let appDel: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDel.persistentContainer.viewContext
+        let edit:String = userText.text!
+        let pass:String = passwordText.text!
+        let newuser:String = newUserText.text!
+        let newpass:String = newPassText.text!
         do {
             let request = NSFetchRequest<NSManagedObject>(entityName: "Users")
             let results = try context.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
@@ -46,16 +54,25 @@ class ViewController: UIViewController {
                 for items in results as! [NSManagedObject] {
                     let name = items.value(forKeyPath: "usernames")
                     let password = items.value(forKeyPath: "password")
-                    print(name!, password!)
+                    if name as! String == edit && password as! String == pass{
+                        //edit data where usernames="justin", set to "justinus"
+                        items.setValue(newuser, forKeyPath: "usernames")
+                        items.setValue(newpass, forKey: "password")
+                        //don't forget to save
+                        do {
+                            try context.save()
+                        } catch {
+                            print("ada error")
+                        }
+                    }
                 } //end of for items
             } //end of if
         } catch {
             print("ada error")
         }
-        userText.text = "" //reset isi textfield untuk isian berikutnya
-        passwordText.text = ""  //reset isi textfield untuk isian berikutnya
+        isi()
+        table.reloadData()
     }
-
     @IBAction func deleteButton(_ sender: UIButton) {
         guard let appDel: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
@@ -83,23 +100,63 @@ class ViewController: UIViewController {
         } catch {
             print("ada error")
         }
+        isi()
+        table.reloadData()
     }
-
-    @IBAction func editButton(_ sender: UIButton) {
+    func isi()->Void{
+        userArr.removeAll()
+        //retrieve the data
         guard let appDel: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
         let context = appDel.persistentContainer.viewContext
-        
+        do {
+            let request = NSFetchRequest<NSManagedObject>(entityName: "Users")
+            let results = try context.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
+            if results.count > 0 {
+                for items in results as! [NSManagedObject] {
+                    let name = items.value(forKeyPath: "usernames") as! String
+                    let pass = items.value(forKeyPath: "password") as! String
+                    userArr.append(users(name,pass))
+                    print(name, pass)
+                } //end of for items
+            } //end of if
+        } catch {
+            print("ada error")
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userArr.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        let i = indexPath.row
+        cell?.textLabel?.text = userArr[i].username
+        cell?.detailTextLabel?.text = userArr[i].password
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard let appDel: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDel.persistentContainer.viewContext
+        let userdel:String = userArr[indexPath.row].username
+        let passdel:String = userArr[indexPath.row].password
         do {
             let request = NSFetchRequest<NSManagedObject>(entityName: "Users")
             let results = try context.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
             if results.count > 0 {
                 for items in results as! [NSManagedObject] {
                     let name = items.value(forKeyPath: "usernames")
-                    if name as! String == "justin" {
-                        //edit data where usernames="justin", set to "justinus"
-                        items.setValue("justinus", forKeyPath: "usernames")
+                    let pass = items.value(forKeyPath: "password")
+                    if name as! String == userdel && pass as! String == passdel {
+                        //delete
+                        context.delete(items)
                         //don't forget to save
                         do {
                             try context.save()
@@ -112,8 +169,8 @@ class ViewController: UIViewController {
         } catch {
             print("ada error")
         }
-
+        isi()
+        table.reloadData()
     }
-    
-}
 
+}
---
+++ coredata_2/users.swift
@@ -0,0 +1,17 @@
+//
+//  users.swift
+//  coredata_2
+//
+//  Created by IOS on 4/13/17.
+//  Copyright © 2017 IOS. All rights reserved.
+//
+
+import Foundation
+class users{
+    var username:String
+    var password:String
+    init(_ a:String, _ b:String){
+        username=a
+        password=b
+    }
+}
